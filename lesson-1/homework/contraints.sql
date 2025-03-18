e01b7ce15fc2cdbbf958dcf2080b363f9a131cdc
sevarapp <sevarapulatova081@gmail.com>
2025-03-18 17:14:48 -0500

    Update contraints.sql

--- lesson-1/homework/contraints.sql
+++ lesson-1/homework/contraints.sql
@@ -1,1 +1,9 @@
-select 1
+create DATABASE HomeworkDB
+create table Students (StudentID int,FullName varchar(50) ,Age int, GPA decimal(3,2))
+Alter table Students
+add Email varchar(50)
+EXEC sp_rename 'Students.FullName', 'Name', 'COLUMN';
+Alter table Students
+drop column age
+truncate table students
+select * from Students
